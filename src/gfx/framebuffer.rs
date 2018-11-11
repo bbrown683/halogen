63c48c3249c42bd123689a5900db9eea0f252274
Ben Brown <bbrown683@live.com>
2018-11-10 19:51:49 -0600

    WIP: Framebuffer.

--- src/gfx/framebuffer.rs
+++ src/gfx/framebuffer.rs
@@ -1,7 +1,8 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 use hal::{Backend, Device as DeviceTrait};
-use hal::image::Extent;
+use hal::format::{Aspects, Format, Swizzle};
+use hal::image::{Extent, SubresourceRange, ViewKind};
 use hal::pso::Viewport;
 use crate::gfx::{Device, RenderPass};
 
@@ -9,34 +10,40 @@ pub struct Framebuffer<B: Backend> {
     device : Rc<RefCell<Device<B>>>,
     render_pass : Rc<RefCell<RenderPass<B>>>,
     framebuffer : Option<B::Framebuffer>,
-    frame_image : Option<B::Image>,
+    image_views : Option<Vec<B::ImageView>>,
 }
 
 impl<B: Backend> Drop for Framebuffer<B> {
     fn drop(&mut self) {
+
         println!("Dropped Framebuffer")
     }
 }
 
 impl<B: Backend> Framebuffer<B> {
-    /*
-    fn new(device : Rc<RefCell<Device<B>>>, render_pass : Rc<RefCell<RenderPass<B>>>, ) -> Self {
-        let framebuffer = device
+    fn new(device : Rc<RefCell<Device<B>>>, render_pass : Rc<RefCell<RenderPass<B>>>,
+           images : &Vec<B::Image>, extent : Extent) -> Self {
+        let resources = SubresourceRange {
+            aspects: Aspects::all(),
+            levels: 0..1,
+            layers: 0..1
+        };
+
+        let mut image_views = Vec::<B::ImageView>::new();
+        for i in 0..images.len() {
+            image_views.push(device
+                .borrow()
+                .get_logical_device()
+                .create_image_view(images.get(i).unwrap(), ViewKind::D2, Format::Bgra8Srgb,
+                                   Swizzle::NO, resources.clone())
+                .expect("Failed to create image view."));
+        }
+        let framebuffer = Some(device
             .borrow()
             .get_logical_device()
             .create_framebuffer(render_pass.borrow().get_render_pass().as_ref().unwrap(),
-                render_pass.borrow().get_attachments(), extent);
-        Self { device, render_pass, framebuffer }
-    }
-    */
-}
-
-pub struct FramebufferBuilder {
-
-}
-
-impl FramebufferBuilder {
-    pub fn with_extent(extent : Extent) {
-
+                                image_views.as_slice(),
+                                extent).expect("Failed to create Framebuffer"));
+        Self { device, render_pass, framebuffer, image_views: Some(image_views) }
     }
 }
